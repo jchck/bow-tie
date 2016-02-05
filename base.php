e4c1d9bebc647d6e5cd97c1c52c3a52ec3549c11
jchck <hey@justinchick.com>
2016-02-05 20:15:03 +0200

    refractor

--- base.php
+++ base.php
@@ -9,45 +9,35 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <?php
-      do_action('get_header');
-      get_template_part('templates/header');
-    ?>
-    <div class="wrap container-fluid" role="document">
+
+    <div class="wrap container-fluid" rold="document">
+      <?php 
+        do_action( 'get_header' ); 
+        get_template_part( 'templates/header' );
+      ?>
       <div class="content row">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
-        <?php if (Setup\display_sidebar()) : ?>
-          <aside class="sidebar">
-            <?php include Wrapper\sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
+        <?php if ( Setup\display_sidebar() ) : ?>
+        <aside class="sidebar">
+          <?php include Wrapper\sidebar_path(); ?>
+        </aside><!-- /.sidebar -->
         <?php endif; ?>
       </div><!-- /.content -->
-    </div><!-- /.wrap -->
+    </div><!-- /.wrap .container-fluid -->
+  
     <?php
       do_action('get_footer');
       get_template_part('templates/footer');
       wp_footer();
+      get_template_part( 'templates/google', 'font' );
     ?>
-    <script type="text/javascript">
-      WebFontConfig = {
-        google: { families: [ 'Open+Sans:400,400italic,700,600:latin' ] }
-      };
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })(); 
-    </script>
   </body>
 </html>
