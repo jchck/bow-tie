7865fa46e19bf943c55febe6e67cbc2fd04ad243
jchck <hey@justinchick.com>
2016-02-05 23:00:46 +0200

    front page first section

--- front-page.php
+++ front-page.php
@@ -2,10 +2,10 @@
 	<div class="col-sm-6">
 		<div class="border text-center">
 			<h2>A WordPress theme that is so fresh and so clean</h2>
-			<a href="#" class="btn btn-default">Download</a>
-			<a href="#" class="btn btn-default">Fork</a>
+			<a href="#" class="btn btn-black btn-lg">Download</a>
+			<a href="#" class="btn btn-black btn-lg">Fork</a>
 			<h3>Made with care by:</h3>
-			<h4>Justin Chick</h4>
+			<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer" class="btn btn-transparent">Justin Chick</a>
 		</div>
 	</div>
 </div>
