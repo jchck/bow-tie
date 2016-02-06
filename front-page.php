32c0f74d128f155cad8a8ca718bb7621480abfc5
jchck <hey@justinchick.com>
2016-02-06 22:49:14 +0200

    front-page footer

--- front-page.php
+++ front-page.php
@@ -77,3 +77,12 @@
 		</div>
 	</div>
 </div>
+
+<div class="row center-xs">
+	<div class="col-xs-6">
+		<div class="box box-black clearfix">
+			<h5 class="pull-left"><a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a></h5>
+			<h5 class="pull-right"><a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Who made this?</a></h5>
+		</div>
+	</div>
+</div>
