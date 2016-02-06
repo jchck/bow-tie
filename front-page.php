71c74d5da50e2b5c6faf0d70a2556d95f3523b3d
jchck <hey@justinchick.com>
2016-02-06 22:23:30 +0200

    front page about and pic sections

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<div class="row center-xs gutter-x-padding section-top">
+<div class="row center-xs gutter-x-padding section">
 	<div class="col-sm-6 col-xs-12">
 		<div class="border text-center">
 			<h2>A WordPress theme that is so fresh and so clean</h2>
@@ -10,7 +10,7 @@
 	</div>
 </div>
 
-<div class="row section-mid">
+<div class="row section">
 	<div class="col-xs-12">
 		<div class="box box-black">
 			<div class="row center-xs">
@@ -40,15 +40,38 @@
 	</div>
 </div>
 
-<div class="row section-bot">
-	<div class="col-sm-6">
-		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/farm.jpg) center; background-size: cover">
+<div class="row center-xs section">
+	<div class="col-xs-8">
+		<div class="box box-white box-border">
+			<div class="about">
+				<p>Bow Tie is a WordPress theme made front-end developer and digital nomad Justin Chick.</p>
+				<a href="#" class="btn btn-black base my1">What Makes This So Great?</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row section">
+	<div class="col-sm-6 p0">
+		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/pic1.jpg) center; background-size: cover">
 			<h2>The Name</h2>
 			<a href="#" class="btn btn-black">View</a>
 		</div>
 	</div>
-	<div class="col-sm-6">
-		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/farm.jpg) center; background-size: cover">
+	<div class="col-sm-6 p0">
+		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/pic2.jpg) center; background-size: cover">
+			<h2>The Name</h2>
+			<a href="#" class="btn btn-black">View</a>
+		</div>
+	</div>
+	<div class="col-sm-6 p0">
+		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/pic3.jpg) center; background-size: cover">
+			<h2>The Name</h2>
+			<a href="#" class="btn btn-black">View</a>
+		</div>
+	</div>
+	<div class="col-sm-6 p0">
+		<div class="pic box-border" style="background: url(/content/themes/bow-tie/dist/images/pic4.jpg) center; background-size: cover">
 			<h2>The Name</h2>
 			<a href="#" class="btn btn-black">View</a>
 		</div>
