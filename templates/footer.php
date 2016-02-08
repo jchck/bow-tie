314f19333da1c2408d7fdcbb1bf0f54ef076fc2a
jchck <hey@justinchick.com>
2016-02-08 12:19:29 +0200

    turn off padding on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,14 @@
 <footer class="col-xs-12">
-	<div class="box box-black clearfix">
-		<h5 class="pull-left">
-			<a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a>
-		</h5>
-		<h5 class="pull-right">
-			<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Who made this?</a>
-		</h5>
+	<div class="box box-black clearfix pr0 pl0">
+		<div class="row center-xs">
+			<div class="col-xs-11">
+				<h5 class="pull-left">
+					<a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a>
+				</h5>
+				<h5 class="pull-right">
+					<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Who made this?</a>
+				</h5>
+			</div>
+		</div>
 	</div>
 </footer>
