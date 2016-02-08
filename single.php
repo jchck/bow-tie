c9e89e64484013fc735cbba697c6c31cf0023845
jchck <hey@justinchick.com>
2016-02-08 16:16:59 +0200

    style category links

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <div class="bg-white">
 	<div class="row center-xs">
-		<div class="col-xs-10">
+		<div class="col-xs-11">
 
 			<?php get_template_part('templates/content-single', get_post_type()); ?>
 
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,3 +1,12 @@
+<?php 
+	$categoryArgs = array(
+		'title_li' => '' 
+	); 
+?>
+
 <div class="meta">
 	<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date('M j, Y'); ?></time>
+	<ul class="list-inline mb0">
+		<?php wp_list_categories( $categoryArgs ); ?>
+	</ul>
 </div>
