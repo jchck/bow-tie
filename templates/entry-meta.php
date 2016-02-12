3145e855ff0ed255b6907208e9bb3b3b8f7e7e11
jchck <hey@justinchick.com>
2016-02-12 13:36:57 +0200

    cleaning up

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,13 +1,3 @@
-<?php 
-	$categoryArgs = array(
-		'title_li' => '' 
-	); 
-?>
-
 <div class="meta text-right">
-	
-	<ul class="list-inline mb0 ml0">
-		<?php wp_list_categories( $categoryArgs ); ?>
-	</ul>
 	<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date('M j, Y'); ?></time>
 </div>
