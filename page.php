2a887a7238880c27b0d270e358935c7f4803b0cb
jchck <hey@justinchick.com>
2016-02-12 11:44:12 +0200

    basic page template

--- page.php
+++ page.php
@@ -1,4 +1,8 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<div class="row center-xs">
+	<div class="col-xs-11">
+		<?php while (have_posts()) : the_post(); ?>
+		<?php get_template_part('templates/page', 'header'); ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+		<?php endwhile; ?>
+	</div>
+</div>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
-</div>
+<header class="flex center-xs">
+  <h1 class="entry-title"><?= Titles\title(); ?></h1>
+</header>
