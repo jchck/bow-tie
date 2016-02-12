69b83b9366ab9a11cb189535d1738df5146385bd
jchck <hey@justinchick.com>
2016-02-12 12:26:28 +0200

    stylizing the base post template

--- template-bio.php
+++ template-bio.php
@@ -3,8 +3,11 @@
  * Template Name: Biography Template
  */
 ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<div class="row center-xs">
+	<div class="col-xs-11">
+		<?php while (have_posts()) : the_post(); ?>
+		  <?php get_template_part('templates/page', 'header'); ?>
+		  <?php get_template_part('templates/content', 'page'); ?>
+		<?php endwhile; ?>
+	</div>
+</div>
