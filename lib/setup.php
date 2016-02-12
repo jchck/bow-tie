42893b32f1a6c717561bbc2f29323899e4d88807
jchck <hey@justinchick.com>
2016-02-12 13:25:24 +0200

    set post thumbnail size

--- lib/setup.php
+++ lib/setup.php
@@ -35,6 +35,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
+  set_post_thumbnail_size( 1020, 900 );
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
