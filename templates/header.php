b4de0e88349d2753876615d30963eaa026c0ea7b
jchck <hey@justinchick.com>
2016-03-01 14:17:20 +0200

    responsive header

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <header class="row center-xs bg-white">
-  <div class="col-xs-12 col-sm-11 flex between-xs middle-xs">
+  <div class="col-xs-12 col-sm-11 flex between-sm middle-xs center-xs">
     <h1><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
     <nav class="nav-primary">
       <?php
