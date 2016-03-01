1e11609b08f155d869723c5904f4542ecf5ffd1e
jchck <hey@justinchick.com>
2016-03-01 14:54:09 +0200

    write function to attach content into the_content

--- lib/share.php
+++ lib/share.php
@@ -2,8 +2,24 @@
 
 namespace Roots\Sage\Share;
 
-function share ( $content ){
+/**
+ *
+ * Add image before the_content() if is_single()
+ * 
+ * @uses @is_single
+ *
+ */
 
+
+function share ( $content ){
+	if (is_single()) {
+		$content = sprintf(
+            '<img class="post-icon" src="%s/dist/images/pic1.jpg" />%s',
+            get_bloginfo( 'stylesheet_directory' ),
+            $content
+        );
+	}
+	return $content;
 }
 
-add_action( 'the_content', __NAMESPACE__ . '\\share');
+add_action( 'the_content', __NAMESPACE__ . '\\share', 20);
