89c761eb57709e7045483ab51cf7951d8ac9d443
jchck <hey@justinchick.com>
2016-03-01 12:13:37 +0200

    center content in footer on mobile

--- templates/footer.php
+++ templates/footer.php
@@ -2,10 +2,10 @@
 	<div class="box box-black clearfix pr0 pl0">
 		<div class="row center-xs">
 			<div class="col-xs-11">
-				<h5 class="pull-left">
+				<h5 class="left">
 					<a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a>
 				</h5>
-				<h5 class="pull-right">
+				<h5 class="right">
 					<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Who made this?</a>
 				</h5>
 			</div>
