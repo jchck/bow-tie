96636cfa1e2b6fef9ef25cb705569a173e733395
jchck <hey@justinchick.com>
2016-03-01 11:33:41 +0200

    page styles

--- page.php
+++ page.php
@@ -1,8 +1,10 @@
 <div class="row center-xs">
-	<div class="col-xs-11">
-		<?php while (have_posts()) : the_post(); ?>
-		<?php get_template_part('templates/page', 'header'); ?>
-		<?php get_template_part('templates/content', 'page'); ?>
-		<?php endwhile; ?>
+	<div class="col-xs-12 col-md-8">
+		<div class="entry-content bg-black">
+			<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('templates/page', 'header'); ?>
+			<?php get_template_part('templates/content', 'page'); ?>
+			<?php endwhile; ?>
+		</div>
 	</div>
 </div>
