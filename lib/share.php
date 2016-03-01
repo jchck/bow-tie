92849c66b225b637f9b02b14c7539cf6fb80555e
jchck <hey@justinchick.com>
2016-03-01 16:01:15 +0200

    2 functions needed to add something into the_content

--- lib/share.php
+++ lib/share.php
@@ -2,24 +2,40 @@
 
 namespace Roots\Sage\Share;
 
+function after_paragraph( $insertion, $paragraph_id, $content ){
+	$closing_p		= '</p>';
+	$paragraphs		= explode( $closing_p, $content );
+
+	foreach ($paragraphs as $index => $paragraph) {
+
+		if (trim( $paragraph )) {
+			$paragraphs[$index]		.= $closing_p;
+		}
+
+		if ($paragraph_id == $index + 1) {
+			$paragraphs[$index] 	.= $insertion;
+		}
+
+	}
+
+	return implode( '', $paragraphs );
+}
+
 /**
  *
  * Add image before the_content() if is_single()
  * 
- * @uses @is_single
+ * @uses @is_single()
  *
  */
+function insert_share( $content ){
+	$share = '<p class="text-center">Yo!</p>';
 
-
-function share ( $content ){
 	if (is_single()) {
-		$content = sprintf(
-            '<img class="post-icon" src="%s/dist/images/pic1.jpg" />%s',
-            get_bloginfo( 'stylesheet_directory' ),
-            $content
-        );
+		return after_paragraph( $share, 2, $content );
 	}
+
 	return $content;
 }
 
-add_action( 'the_content', __NAMESPACE__ . '\\share', 20);
+add_action( 'the_content', __NAMESPACE__ . '\\insert_share', 20);
