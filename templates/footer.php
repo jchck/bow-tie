617ef6ec9b1a412087e49b5782adda9d65cbc269
jchck <hey@justinchick.com>
2016-03-09 17:29:02 +0200

    put a sexy bow tie in the footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,11 @@
 <footer class="col-xs-12">
 	<div class="box box-black clearfix pr0 pl0">
 		<div class="row center-xs">
+			<a class="bow-tie" href="<?= esc_url(home_url('/')); ?>">
+				<?php get_template_part( 'templates/bow', 'tie' ); ?>
+			</a>
+		</div>
+		<div class="row center-xs">
 			<div class="col-xs-11">
 				<h5 class="left">
 					<a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a>
