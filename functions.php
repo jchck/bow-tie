cfcc4cdb2d4c60ccb4fdb1b333503c1fce9e93e4
jchck <hey@justinchick.com>
2016-03-09 17:06:25 +0200

    turn off social sharing for now

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ $sage_includes = [
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php', // Theme customizer
-  'lib/share.php'
+  //'lib/share.php'
 ];
 
 foreach ($sage_includes as $file) {
