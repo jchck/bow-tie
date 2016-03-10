2105ea2039c7f7ac0a7df31ec6c80c8b218203e2
jchck <hey@justinchick.com>
2016-03-10 13:10:06 +0200

    coloring

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,5 @@
 <div class="col-sm-4">
-	<article <?php post_class(); ?>>
+	<article <?php post_class('box box-white my1'); ?>>
 		<header class="flex center-xs" style="background: url(<?php the_post_thumbnail_url(); ?>) center center; background-size: cover;">
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
