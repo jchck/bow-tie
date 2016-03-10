315768e85824dcd90e22a84260003500986453d3
jchck <hey@justinchick.com>
2016-03-10 17:10:11 +0200

    style if no post thumbnail

--- templates/content.php
+++ templates/content.php
@@ -1,6 +1,6 @@
 <div class="col-sm-4">
 	<a href="<?php the_permalink(); ?>" <?php post_class('box box-white my1'); ?>>
-		<header class="flex center-xs" style="background: url(<?php the_post_thumbnail_url(); ?>) center center; background-size: cover;">
+		<header class="flex center-xs" <?php if(has_post_thumbnail()): ?> style="background: url(<?php the_post_thumbnail_url(); ?>) center center; background-size: cover;"<?php endif; ?>>
 			<h2 class="entry-title"><?php the_title(); ?></h2>
 		</header>
 		<div class="entry-summary">
