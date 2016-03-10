1c9d7e277d2539323d0d29ecb08282c9f2752080
jchck <hey@justinchick.com>
2016-03-10 14:37:41 +0200

    what to do, what to do

--- templates/content.php
+++ templates/content.php
@@ -1,10 +1,10 @@
 <div class="col-sm-4">
-	<article <?php post_class('box box-white my1'); ?>>
+	<a href="<?php the_permalink(); ?>" <?php post_class('box box-white my1'); ?>>
 		<header class="flex center-xs" style="background: url(<?php the_post_thumbnail_url(); ?>) center center; background-size: cover;">
-			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<h2 class="entry-title"><?php the_title(); ?></h2>
 		</header>
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
 		</div>
-	</article>
+	</a>
 </div>
