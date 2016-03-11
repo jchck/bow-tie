4d3f15a1cf8220d5dd21df7bc3ad0bad2df75baa
jchck <hey@justinchick.com>
2016-03-11 13:30:03 +0200

    mobile QA: index.php

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,4 @@
-<div class="col-sm-4">
+<div class="col-xs-11 col-sm-6 col-md-4 flex">
 	<a href="<?php the_permalink(); ?>" <?php post_class('box box-white my1'); ?>>
 		<header class="flex center-xs" <?php if(has_post_thumbnail()): ?> style="background: url(<?php the_post_thumbnail_url(); ?>) center center; background-size: cover;"<?php endif; ?>>
 			<h2 class="entry-title"><?php the_title(); ?></h2>
