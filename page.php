1b149ac6e177761d54e0cc00ac74a5872d6528c1
jchck <hey@justinchick.com>
2016-03-11 12:53:26 +0200

    mobile QA: page.php

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <div class="row center-xs">
-	<div class="col-xs-12 col-md-8">
+	<div class="col-xs-11 col-md-8">
 		<div class="entry-content bg-black">
 			<?php while (have_posts()) : the_post(); ?>
 			<?php get_template_part('templates/page', 'header'); ?>
