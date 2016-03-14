d65f0997563a8f768afd12c26fc9bd6b957c57b0
jchck <hey@justinchick.com>
2016-03-14 14:58:17 +0200

    yeah that mobile nav, son

--- templates/header.php
+++ templates/header.php
@@ -1,27 +1,5 @@
-<nav class="navbar navbar-default">
-
-  <div clas="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-  </div>
-
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <?php 
-    if (has_nav_menu( 'primary_navigation' )) :
-        wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'] );
-    endif;
-    ?>
-  </div>
-
-</nav>
-
-<!-- <header class="row center-xs bg-white">
-  <div class="col-xs-12 col-sm-11 flex between-sm middle-xs center-xs navbar navbar-default">
+<header class="row center-xs bg-white">
+  <div class="col-xs-12 col-sm-11 navbar navbar-default mb0">
 
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -33,13 +11,12 @@
       <h1><a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
     </div>
 
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
       endif;
       ?>
     </div>
   </div>
 </header>
- -->
