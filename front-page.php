5cdeb4dc13d40375a11658a41d34b59d1bac48f7
jchck <hey@justinchick.com>
2016-04-07 11:07:17 -0400

    responsive QA top section front page

--- front-page.php
+++ front-page.php
@@ -7,8 +7,8 @@
 				<div class="col-md-6 col-sm-8 col-xs-12">
 					<div class="border text-center">
 						<h2>A WordPress theme that is so fresh and so clean</h2>
-						<a href="https://github.com/jchck/bow-tie/archive/master.zip" class="btn btn-black btn-lg">Download</a>
-						<a href="https://github.com/jchck/bow-tie" target="_blank" class="btn btn-black btn-lg">Fork</a>
+						<a href="https://github.com/jchck/bow-tie/archive/master.zip" class="hidden-xs btn btn-black btn-lg">Download</a>
+						<a href="https://github.com/jchck/bow-tie" target="_blank" class="hidden-xs btn btn-black btn-lg">Fork</a>
 						<h3>Made with care by:</h3>
 						<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer" class="btn btn-transparent">Justin Chick</a>
 					</div>
