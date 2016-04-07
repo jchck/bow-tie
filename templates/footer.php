f193465159b3a51758de7f8cb6568361226d8259
jchck <hey@justinchick.com>
2016-04-07 09:54:43 -0400

    footer copy

--- templates/footer.php
+++ templates/footer.php
@@ -14,7 +14,7 @@
 					<a href="<?php echo admin_url(); ?>" target="_blank" class="login">&copy;</a> <?php echo date('Y') ?> <a href="/"><?php bloginfo( 'name' ); ?></a>
 				</h5>
 				<h5 class="right">
-					<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Who made this?</a>
+					<a href="http://jstn.ch/ck" target="_blank" title="Justin Chick is a freelance front-end developer">Made by Justin Chick</a>
 				</h5>
 			</div>
 		</div>
